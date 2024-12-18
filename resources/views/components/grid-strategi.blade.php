748a78fa7810655bdfef806c097761355c3427e7
ALcitya <ichwanalif55@gmail.com>
2024-12-18 12:10:09 +0700

    menambak komponent grid

---
+++ resources/views/components/grid-strategi.blade.php
@@ -0,0 +1,1 @@
+<div class="rounded-lg box-border h-12 p-3 border-1 bg-indigo-500 w-96">{{ $slot }}</div>
--- resources/views/data-strategi.blade.php
+++ resources/views/data-strategi.blade.php
@@ -8,5 +8,19 @@
 <body>
   <x-navbar></x-navbar>
   <x-header>Data Strategi Digital Marketing</x-header>
+  <main class="flex justify-center">
+    <div class="grid grid-cols-2 gap-5">
+      <x-grid-strategi>SEO (Search Engine Optimization)</x-grid-strategi>
+      <x-grid-strategi>Social Media Marketing</x-grid-strategi>
+      <x-grid-strategi>content Marketing</x-grid-strategi>
+      <x-grid-strategi>influencer Marketing</x-grid-strategi>
+      <x-grid-strategi>Affiliate Marketing</x-grid-strategi>
+      <x-grid-strategi>Video Marketing</x-grid-strategi>
+      <x-grid-strategi>Web Analytics</x-grid-strategi>
+      <x-grid-strategi>Remarketing</x-grid-strategi>
+      <x-grid-strategi>Online Public Relations</x-grid-strategi>
+      <x-grid-strategi>Pay-Per-Click Advertising (PPC)</x-grid-strategi>
+    </div>
+  </main>
 </body>
 </html>
